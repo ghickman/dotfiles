86141933f19c52349823055fa3d54ce16044b4c3
George Hickman <george@ghickman.co.uk>
2011-09-19 20:37:46 +0100

    skip files already symlinked

--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,12 @@
 from os import listdir, symlink
-from os.path import abspath, expanduser
+from os.path import abspath, expanduser, join
 
 for fn in listdir('.'):
-    if fn == '.git':
+    if fn == '.git' or not fn.startswith('.'):
         continue
-    symlink(abspath(fn), expanduser('~'))
+    try:
+        symlink(abspath(fn), join(expanduser('~/'), fn))
+    except OSError:
+        print '%s already exists, skipping.' % fn
+        pass
 
