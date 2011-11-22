b50e362330c1683d917b924d7b8420b2ef8ba895
George Hickman <george@icarium.local>
2011-11-22 21:26:05 +0000

    Can ignore just dotfiles when symlinking now

--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,8 @@
 from os import listdir, symlink
-from os.path import abspath, expanduser, join
+from os.path import abspath, basename, expanduser, join
 
 for fn in listdir('.'):
-    if fn == '.git' or not fn.startswith('.'):
+    if not fn.startswith('.') and fn != basename(__file__):
         continue
     try:
         symlink(abspath(fn), join(expanduser('~/'), fn))
