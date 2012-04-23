32e089e5182564fe05da24c585dacd9abf7a1f56
George Hickman <george@ghickman.co.uk>
2012-04-23 22:28:27 +0100

    Add postactivate hook and update bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -1,15 +1,18 @@
 from os import listdir, symlink, system
-from os.path import abspath, basename, expanduser, join
+from os.path import abspath, basename, expanduser, isdir, join
 
-for fn in listdir('.'):
-    if fn.startswith('.') or fn is basename(__file__):
-        continue
-    try:
-        symlink(abspath(fn), join(expanduser('~/'), '.%s' % fn))
-    except OSError:
-        print '%s already exists, skipping.' % fn
-        pass
 
-    if fn == 'osx':
-        system('/bin/sh ~/.osx')
+def link(files, dest):
+    for fn in files:
+        if not (fn.startswith('.') or fn is basename(__file__) or isdir(fn)):
+            continue
+        try:
+            symlink(abspath(fn), join(expanduser(dest), '.{0}'.format(fn)))
+        except OSError:
+            print('{0} already exists, skipping.'.format(fn))
+
+link([fn for fn in listdir('.') if fn != 'osx' or not isdir(fn)], '~/')
+link([fn for fn in listdir('./virtualenv')], '~/.venvs')
+
+system('/bin/sh ~/.osx')
 
