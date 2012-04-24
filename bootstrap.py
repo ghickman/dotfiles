43bd16d8911c8407362acad5d5315925d826d89d
George Hickman <george@ghickman.co.uk>
2012-04-24 10:11:40 +0100

    Remove all the virtualenv gunf, sack that off for autoenv

--- bootstrap.py
+++ bootstrap.py
@@ -1,18 +1,14 @@
 from os import listdir, symlink, system
-from os.path import abspath, basename, expanduser, isdir, join
+from os.path import abspath, basename, expanduser, join
 
 
-def link(files, dest):
-    for fn in files:
-        if not (fn.startswith('.') or fn is basename(__file__) or isdir(fn)):
-            continue
-        try:
-            symlink(abspath(fn), join(expanduser(dest), '.{0}'.format(fn)))
-        except OSError:
-            print('{0} already exists, skipping.'.format(fn))
-
-link([fn for fn in listdir('.') if fn != 'osx' or not isdir(fn)], '~/')
-link([fn for fn in listdir('./virtualenv')], '~/.venvs')
+for fn in listdir('.'):
+    if not (fn.startswith('.') or fn is basename(__file__) or fn == 'osx'):
+        continue
+    try:
+        symlink(abspath(fn), join(expanduser('~/'), '.{0}'.format(fn)))
+    except OSError:
+        print('{0} already exists, skipping.'.format(fn))
 
 system('/bin/sh ~/.osx')
 
