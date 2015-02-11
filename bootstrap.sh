59fa4eb1ea065b9c41e9c6ceeb9aa7c75e4f85c7
George Hickman <george@ghickman.co.uk>
2015-02-11 10:13:19 +0000

    Do the update homebrew installer dance

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@ sudo /usr/bin/xcodebuild -license
 
 echo "========== install homebrew"
 if ! hash brew 2>/dev/null; then
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo "========== brew the world"
