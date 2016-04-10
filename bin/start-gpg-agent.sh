14d5dad9d999c54e7ccaa7f95624331543d303e4
George Hickman <george@ghickman.co.uk>
2016-04-10 20:35:25 +0100

    Clean up gpg-agent running because gpg is so hot right now

--- bin/start-gpg-agent.sh
+++ bin/start-gpg-agent.sh
@@ -1,8 +1,13 @@
-if test -f $HOME/.gpg-agent-info && \
-  kill -0 `cut -d: -f 2 $HOME/.gpg-agent-info` 2>/dev/null; then
-    GPG_AGENT_INFO=`cat $HOME/.gpg-agent-info`
+#!/bin/sh
+
+set -e
+
+agent_info="$HOME/.gnupg/.gpg-agent-info"
+
+if test -f "$agent_info" && -n "$(pgrep gpg-agent)"; then
+    . "$agent_info"
     export GPG_AGENT_INFO
 else
-  eval `/usr/local/bin/gpg-agent --daemon --write-env-file`
+    eval "$(/usr/local/bin/gpg-agent --daemon --write-env-file "$agent_info")"
 fi
-export GPG_TTY=`tty`
+# export GPG_TTY=$(tty)
