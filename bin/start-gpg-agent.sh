34e7ce2543aeec471da6831c3e3b566788bf7eff
George Hickman <george@ghickman.co.uk>
2017-08-22 10:42:20 +0100

    who even gpgs

--- bin/start-gpg-agent.sh
+++ bin/start-gpg-agent.sh
@@ -4,10 +4,9 @@ set -e
 
 agent_info="$HOME/.gnupg/.gpg-agent-info"
 
-if test -f "$agent_info" && test -n "$(pgrep gpg-agent)"; then
+if test -f "$agent_info" && [ -n "$(pgrep gpg-agent)" ]; then
     . "$agent_info"
     export GPG_AGENT_INFO
 else
-    eval "$(/usr/local/bin/gpg-agent --daemon --write-env-file "$agent_info")"
+    eval $(/usr/local/bin/gpg-agent --daemon "$agent_info")
 fi
-# export GPG_TTY=$(tty)
