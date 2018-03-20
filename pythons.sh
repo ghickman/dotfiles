8e1ddf0666edc7b38991bd3f4d6c6902f1be31a9
George Hickman <george@ghickman.co.uk>
2018-03-20 15:31:41 +0000

    Bump python versions

--- pythons.sh
+++ pythons.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pyenv install 2.7.13
-pyenv install 3.6.1
-pyenv install 3.5.3
-pyenv install 3.4.6
+pyenv install 2.7.14
+pyenv install 3.6.4
+pyenv install 3.5.5
+pyenv install 3.4.8
 pyenv install pypy-5.7.1
 
-pyenv global 2.7.13 3.6.1 3.5.3 3.4.6 pypy-5.7.1
+pyenv global 2.7.14 3.6.4 3.5.5 3.4.8 pypy-5.7.1
