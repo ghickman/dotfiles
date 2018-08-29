794b7750fabdf4dcdebf0bdcb528568647f704a1
George Hickman <george@ghickman.co.uk>
2018-08-29 14:41:16 +0100

    bump to latest pythons and py3k as default!

--- pythons.sh
+++ pythons.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-pyenv install 2.7.14
-pyenv install 3.6.4
-pyenv install 3.5.5
-pyenv install 3.4.8
+pyenv install 2.7.15
+pyenv install 3.5.6
+pyenv install 3.6.6
+pyenv install 3.7.0
 pyenv install pypy-5.7.1
+pyenv install pypy3.5-6.0.0
 
-pyenv global 2.7.14 3.6.4 3.5.5 3.4.8 pypy-5.7.1
+pyenv global 3.7.0 3.6.6 3.5.6 2.7.15 pypy-5.7.1 pypy3.5-6.0.0
