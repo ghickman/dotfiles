38343e9da4eabc7b0c29db653cc663b04cfc45c0
George Hickman <george@ghickman.co.uk>
2019-04-29 09:04:47 +0100

    bump pyenv versions

--- pythons.sh
+++ pythons.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-pyenv install 2.7.15
-pyenv install 3.5.6
-pyenv install 3.6.6
-pyenv install 3.7.0
+pyenv install 2.7.16
+pyenv install 3.5.7
+pyenv install 3.6.8
+pyenv install 3.7.3
 pyenv install pypy-5.7.1
-pyenv install pypy3.5-6.0.0
+pyenv install pypy3.6-7.1.0
 
-pyenv global 3.7.0 3.6.6 3.5.6 2.7.15 pypy-5.7.1 pypy3.5-6.0.0
+pyenv global 3.7.3 3.6.8 3.5.7 2.7.16 pypy-5.7.1 pypy3.6-7.1.0
