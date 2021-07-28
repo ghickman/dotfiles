cdc9a0b24076e541aa1a41ce416078b32a2830d4
George Hickman <george@ghickman.co.uk>
2021-07-28 16:18:58 +0100

    emblacken offlineimap keychain script

--- mutt/offlineimap.py
+++ mutt/offlineimap.py
@@ -7,14 +7,17 @@ import subprocess
 
 def get_keychain_pass(account=None, server=None):
     params = {
-        'security': '/usr/bin/security',
-        'command': 'find-internet-password',
-        'account': account,
-        'server': server,
-        'keychain': os.path.expanduser('~/Library/Keychains/login.keychain'),
+        "security": "/usr/bin/security",
+        "command": "find-internet-password",
+        "account": account,
+        "server": server,
+        "keychain": os.path.expanduser("~/Library/Keychains/login.keychain"),
     }
-    command = "sudo -u george %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    command = (
+        "sudo -u george %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s"
+        % params
+    )
     output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
-    outtext = [l for l in output.splitlines() if l.startswith('password: ')][0]
+    outtext = [line for line in output.splitlines() if line.startswith("password: ")][0]
 
     return re.match(r'password: "(.*)"', outtext).group(1)
