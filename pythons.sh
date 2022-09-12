6fb76bb2a7ad33ede03d3a4900ef763c81500bc1
George Hickman <george@ghickman.co.uk>
2022-09-12 22:19:27 +0100

    update pythons

--- pythons.sh
+++ pythons.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-pyenv install 3.9.1
-pyenv install 3.8.1
-pyenv install 3.7.3
-pyenv install 3.6.8
-pyenv install 2.7.18
-pyenv install pypy-5.7.1
-pyenv install pypy3.6-7.1.0
+pyenv install 3.10.4
+pyenv install 3.9.12
+pyenv install 3.8.12
+pyenv install 3.7.12
 
-pyenv global 3.7.3 3.6.8 3.5.7 2.7.16 pypy-5.7.1 pypy3.6-7.1.0
+pyenv global 3.10.4 3.9.12 3.8.12 3.7.12
