589ac97e8fd2860e6d1037cdc87daff88c00c69e
George Hickman <george@ghickman.co.uk>
2023-09-03 14:58:37 +0100

    move git config files into ~/.config

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,6 +37,7 @@ ln -s "$PWD/mutt/mime.types" "$HOME/.mime.types"
 ln -s "$PWD/poetry-global-config.toml" "$HOME/Library/Application Support/pypoetry/config.toml"
 ln -s "$PWD/direnvrc" "$HOME/.config/direnv/direnvrc"
 ln -s "$PWD/pyenv-default-packages" "$HOME/.pyenv/default-packages"
+ln -s "$PWD/git" "$HOME/.config/git"
 
 echo "========== set up yaml lint"
 yamllint_dir="$HOME/.config/yamllint"
