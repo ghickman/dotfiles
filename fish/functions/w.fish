7a197ea64c42cda110f2ab4c3677398535e93aea
George Hickman <george@ghickman.co.uk>
2024-04-06 16:49:20 +0100

    Manage multiple projects

--- fish/functions/w.fish
+++ fish/functions/w.fish
@@ -1,3 +1,4 @@
 function w
-    cd (find $HOME/Projects/snowfox -maxdepth 1 -type "d" | fzf) || exit
+    cd (find $HOME/Projects -maxdepth 1 -type "d" | fzf) || exit
+    cd (find . -maxdepth 1 -type "d" | fzf) || exit
 end
