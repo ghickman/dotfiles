73af72623e799597b769f6a81029eb0ae7752611
George Hickman <george@ghickman.co.uk>
2024-10-24 10:12:26 +0100

    Add magic test expanding function

--- fish/abbreviations.fish
+++ fish/abbreviations.fish
@@ -4,4 +4,11 @@ function _expand_run -d "Expand the 'run' abbreviation into a project-specific r
     end
 end
 
+function _expand_test -d "Expand the 'test' abbreviation into a project-specific test command.";
+    if test -e Justfile;
+        echo just test
+    end
+end
+
 abbr --add run --function _expand_run;
+abbr --add test --function _expand_test;
