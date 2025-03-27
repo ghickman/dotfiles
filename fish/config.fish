a580d44303087e9b8d98c5caa93c810307a4c77a
George Hickman <george@ghickman.co.uk>
2025-03-27 09:34:55 +0000

    Add the orbstack CLI tools

--- fish/config.fish
+++ fish/config.fish
@@ -11,3 +11,7 @@ set fish_greeting
 
 . ~/.config/fish/aliases.fish
 . ~/.config/fish/abbreviations.fish
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init.fish 2>/dev/null || :
